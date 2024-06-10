0cfb5898f937031dc8b65c19ee7a8bfd653f46d2
Brian Juma <98020785+briannjuma@users.noreply.github.com>
2024-06-10 21:53:18 +0300

    Pass test: decryption with specific shift key

--- caesar_cipher.py
+++ caesar_cipher.py
@@ -87,3 +87,25 @@ class CaesarCipher:
             encrypted_message += char
 
         return encrypted_message
+
+    def decrypt(self, message) -> str:
+        """Decrypts a message using Caesar Cipher"""
+        uppercase = self.uppercase
+        lowercase = self.lowercase
+        key = self.shift_key
+        decrypted_message = ""
+
+        for char in message:
+            if char in uppercase:
+                index = uppercase.index(char)
+                shift = (index - key) % 26
+                char = uppercase[shift]
+
+            if char in lowercase:
+                index = lowercase.index(char)
+                shift = (index - key) % 26
+                char = lowercase[shift]
+
+            decrypted_message += char
+
+        return decrypted_message
