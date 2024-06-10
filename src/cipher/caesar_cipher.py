060b28df55709445dc32825cea0d08f7a37cb6a2
Brian Juma <98020785+briannjuma@users.noreply.github.com>
2024-06-10 20:43:39 +0300

    Add encryption method with specific shift key

--- src/cipher/caesar_cipher.py
+++ src/cipher/caesar_cipher.py
@@ -1,6 +1,63 @@
 class CaesarCipher:
     """A class for performing Caesar Cipher encryption and decryption"""
 
+    uppercase = [
+        "A",
+        "B",
+        "C",
+        "D",
+        "E",
+        "F",
+        "G",
+        "H",
+        "I",
+        "J",
+        "K",
+        "L",
+        "M",
+        "N",
+        "O",
+        "P",
+        "Q",
+        "R",
+        "S",
+        "T",
+        "U",
+        "V",
+        "W",
+        "X",
+        "Y",
+        "Z",
+    ]
+    lowercase = [
+        "a",
+        "b",
+        "c",
+        "d",
+        "e",
+        "f",
+        "g",
+        "h",
+        "i",
+        "j",
+        "k",
+        "l",
+        "m",
+        "n",
+        "o",
+        "p",
+        "q",
+        "r",
+        "s",
+        "t",
+        "u",
+        "v",
+        "w",
+        "x",
+        "y",
+        "z",
+    ]
+
     def __init__(self, shift_key) -> None:
         """Initializes a CaesarCipher object with a shift value
 
@@ -8,3 +65,25 @@ class CaesarCipher:
         shift_key -shift value(positive for encryption, negative for decryption)
         """
         self.shift_key = shift_key % 26  # Normalize shift value (0-25)
+
+    def encrypt(self, message) -> str:
+        """Encrypts a message using Caesar Cipher"""
+        uppercase = self.uppercase
+        lowercase = self.lowercase
+        key = self.shift_key]
+        encrypted_message = ""
+
+        for char in message.lower():
+            if char in uppercase:
+                index = uppercase.index(char)
+                shift = (index + key) % 26
+                char = uppercase[shift]
+
+            if char in lowercase:
+                index = uppercase.index(char)
+                shift = (index + key) % 26
+                char = lowercase[shift]
+
+            encrypted_message += char
+
+        return encrypted_message
