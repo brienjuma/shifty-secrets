ef8c6c030f37ccca8ac76f34ff1873b6f5a94aed
Brian Juma <98020785+briannjuma@users.noreply.github.com>
2024-06-10 20:04:12 +0300

    Add test for decryption with specific shift key

--- tests/cipher/test_caesar_cipher.py
+++ tests/cipher/test_caesar_cipher.py
@@ -9,6 +9,11 @@ class TestCaesarCipher(unittest.TestCase):
         encrypted_message = cipher.encrypt("hello world")
         self.assertEqual(encrypted_message, "khoor zruog")
 
+    def test_decrypt(self):
+        cipher = CaesarCipher(3)
+        decrypted_message = cipher.decrypt("khoor zruog")
+        self.assertEqual(decrypted_message, "hello world")
+
 
 if __name__ == "__main__":
     unittest.main()
